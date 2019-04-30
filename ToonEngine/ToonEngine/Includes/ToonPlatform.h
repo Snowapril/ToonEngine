48ed8d6216a4e5333db9f65b5c4c837e4f22be4b
snowapril <sinjihng@naver.com>
2019-04-30 16:14:19 +0900

    add cross platform sleep function, at now support only windows and linux

--- ToonEngine/ToonEngine/Includes/ToonPlatform.h
+++ ToonEngine/ToonEngine/Includes/ToonPlatform.h
@@ -7,4 +7,20 @@
 
 #define TOON_KEY_COUNT 104
 
+#ifdef _WIN32
+#include <Windows.h>
+TOON_FORCE_INLINE void SleepCrossPlatform(unsigned long ms) noexcept
+{
+	Sleep(ms);
+}
+#elif __linux__
+#include <unistd.h>
+TOON_FORCE_INLINE void SleepCrossPlatform(unsigned long ms) noexcept
+{
+	unsigned long seconds = ms / static_cast<float>(1000);
+	sleep(seconds);
+}
+#endif
+
+
 #endif
