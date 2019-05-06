cbab11ede336ad03a96c257f6a6a88da38173107
Snowapril <sinjihng@pusan.ac.kr>
2019-05-06 11:33:43 +0900

    Modify ConfigGen.py to generate whole config files.

--- ToonEngine/Scripts/ConfigGen.py
+++ ToonEngine/Scripts/ConfigGen.py
@@ -5,11 +5,19 @@ import configparser
 def main() :
 	current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 	root_path = os.path.join(current_path, "..")
-	target_config_file = os.path.join(root_path, "Resources/engine_config.ini")
+	target_config_file = os.path.join(root_path, "Resources/engine_config2.ini")
 
 	config = configparser.ConfigParser()
-	config.read(target_config_file)
-	config["Common"]["root_path"] = '"{}"'.format(root_path)
+	config['Common'] = {
+		'root_path' : '"{}"'.format(root_path),
+		'log_path' : '"\Log"'
+	}
+	config['RenderSystem'] = {
+		'window_title' : '"ToonEngine Version 0.0.1"',
+		'client_width' : 1200,
+		'client_height' : 900,
+		'default_fullscreen' : 'false'
+	}
 
 	with open(target_config_file, 'w') as ini :
 		config.write(ini)
