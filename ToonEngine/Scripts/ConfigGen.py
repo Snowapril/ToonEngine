f99f7741a7f3dc46fcfe2335e98f3a997bf703fb
Snowapril <sinjihng@pusan.ac.kr>
2019-05-06 11:42:15 +0900

    solve 'not exists' error of module os.

--- ToonEngine/Scripts/ConfigGen.py
+++ ToonEngine/Scripts/ConfigGen.py
@@ -2,26 +2,31 @@ import inspect
 import os
 import configparser
 
-def main() :
-	current_path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-	root_path = os.path.join(current_path, "..")
-	target_config_file = os.path.join(root_path, "Resources/engine_config2.ini")
 
-	config = configparser.ConfigParser()
-	config['Common'] = {
-		'root_path' : '"{}"'.format(root_path),
-		'log_path' : '"\Log"'
-	}
-	config['RenderSystem'] = {
-		'window_title' : '"ToonEngine Version 0.0.1"',
-		'client_width' : 1200,
-		'client_height' : 900,
-		'default_fullscreen' : 'false'
-	}
+def main():
+    current_path = os.path.dirname(os.path.abspath(
+        inspect.getfile(inspect.currentframe())))
+    root_path = os.path.join(current_path, "..")
+    resource_path = os.path.join(root_path, "Resources")
+	
+	# Check resource dir exists, if doesnt exists create one.
+    if not os.path.exists(resource_path) : os.makedirs(resource_path)
 
-	with open(target_config_file, 'w') as ini :
-		config.write(ini)
-		
+    config = configparser.ConfigParser()
+    config['Common'] = {
+        'root_path': '"{}"'.format(root_path),
+        'log_path': r'"\Log"'
+    }
+    config['RenderSystem'] = {
+        'window_title': '"ToonEngine Version 0.0.1"',
+        'client_width': 1200,
+        'client_height': 900,
+        'default_fullscreen': 'false'
+    }
+    config_file_path = os.path.join(resource_path, "engine_config.ini")
+    with open(config_file_path, 'w') as ini:
+        config.write(ini)
+
 
 if __name__ == "__main__" :
-	main()
+    main()
