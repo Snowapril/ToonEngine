dc53c1890c43d1e6d3b3c8fd0589c8208b1dabce
snowapril <sinjihng@pusan.ac.kr>
2019-05-16 15:07:03 +0900

    test bitwise operation

--- ToonEngine/TestSolution/Sources/main.cpp
+++ ToonEngine/TestSolution/Sources/main.cpp
@@ -1,9 +1,16 @@
-#include <ToonEngine/ToonBitwise.h>
+#include <type_traits>
 #include <iostream>
+
+template < typename RetType, typename... Bits, typename = typename std::enable_if_t< std::is_integral_v<RetType>> >
+static auto inline setBitmask(Bits... bits) -> std::decay_t<RetType>
+{
+	RetType mask = ((1 << bits) | ...);
+	return mask;
+}
 
 int main(void)
 {
-	auto a = Toon::Bitwise::setBitmask<unsigned int>(1, 3, 5);
-	std::cout << std::hex << a << std::endl;
+	unsigned int mask = setBitmask<unsigned int>(0, 3, 7, 9);
+	std::cout << std::hex << mask << std::endl;
 	return 0;
 }
